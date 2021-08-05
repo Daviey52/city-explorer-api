e47642faa65917866559903ced0351d2b7fc34da
Daviey52 <davidkariska@gmail.com>
2021-08-04 21:07:37 -0700

    first submits

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
 
 ## Overview
 
-This app servers as a server that has weather data,which it sends whenever requested by te frontend.Importantly it only sends requested data;
+This app servers as a server that has weather data,which it sends whenever requested by te frontend.Importantly it only sends requested data. Add functionality allows the user to view movies related to their typed input name.
 
 ## Getting Started
 
@@ -20,10 +20,13 @@ This app was designed using React Library,react-bootstrap, css, javascript and h
 ## Change Log
 
 08/03/2021 02:00 - The application can now serve as a server that send's requested weather data back to the front end
+08/04/2021 20:57 - The application can now serve as a server for movies which can be searched according to the city name entered
 
 ## Credit and Collaborations
 
-Collaborations: Alex Payne and Brutaism - UML diagram(Lab07)  
+Collaborations: Alex Payne and Brutaism - UML diagram(Lab07)
+
+Collaboration: Quentin Young - UML diagram(Lab07)
 
 ### Name of feature
 
@@ -36,3 +39,5 @@ Finish time:14:00
 Actual time needed to complete: 08:00
 
 ![UML pic](./src/Lab07%20-%20UML.png)
+
+![UML pic](./src/Lab08%20-%20UML.png)
--- server.js
+++ server.js
@@ -7,41 +7,72 @@ app.use(cors());
 require('dotenv').config();
 const PORT = process.env.PORT;
 const weatherData = require('./data/weather.json');
+const axios = require('axios');
+
 
 
 class ForeCast {
-  constructor (description, date) {
-    this.description = description;
-    this.date = date;
+  constructor (day) {
+    this.description = day.weather.description;
+    this.date = day.datetime;
+
+  }
+}
+class Movie {
+  constructor (video) {
+    this.title = video.original_title;
+    this.overview = video.overview;
+    this.average_votes = video.vote_average;
+    this.total_votes = video.vote_count;
+    this.image_url = video.poster_path;
+    this.popularity = video.popularity;
+    this.released_on = video.release_date;
 
   }
 }
 
 
-app.get('/weather', getWeather);
-function getWeather(request, response) {
+app.get('/weather', async (request, response) => {
   console.log('weather hit');
+  //let cityNameRequested = request.query.searchQuery;
+  let lat = request.query.lat;
+  let lon = request.query.lon;
+  console.log(lat, lon);
+
+
+
+  let result = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_MASTER_API_KEY}`);
+
+  // let city = result.find(
+  //   obj => obj.timezone.toLowerCase() === cityNameRequested.toLowerCase());
+  // let forecastArr = [];
+  // if (city) {
+  //   city.data.data.forEach(item => forecastArr.push(new ForeCast(
+  //     `low of ${item.low_temp}, high of ${item.high_temp} with ${item.weather.description}`
+  //     , item.datetime)));
+  //   response.send(forecastArr);
+  // } else {
+  //   response.status(404).send('There is no weather data for the requested city');
+  // }
+
+  response.send(result.data.data.map(item => new ForeCast(item)));
+
+
+
+});
+
+app.get('/movie', async (request, response) => {
   let cityNameRequested = request.query.searchQuery;
-  let latRequested = (request.query.lat);
-  let lonRequested = (request.query.lon);
-  console.log(latRequested, lonRequested, cityNameRequested);
+  console.log(cityNameRequested);
 
-  let cityFound = weatherData.find(
-    obj => obj.city_name.toLowerCase() === cityNameRequested.toLowerCase());
+  console.log('here');
+  let movieResponse = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${cityNameRequested}`);
 
+  console.log(movieResponse.data.results);
+  response.send(movieResponse.data.results.map(obj => new Movie(obj)));
 
-  let forecastArray = [];
+});
 
-  if (cityFound) {
-    cityFound.data.forEach(obj => forecastArray.push(new ForeCast(
-      `low of ${obj.low_temp}, high of ${obj.high_temp} with ${obj.weather.description}`
-      , obj.datetime)));
-    response.send(forecastArray);
-  } else {
-    response.status(404).send('There is no weather data for the requested city');
-  }
-
-}
 
 app.get('*', (request, response) => {
   response.status(500).send('something went wrong');
